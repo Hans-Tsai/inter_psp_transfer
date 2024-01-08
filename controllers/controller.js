0bbac07b9ece2285e483e5768ab271de347f7348
Hans <lgs840522@gmail.com>
2024-01-08 17:35:47 +0800

    feat(controller): add controller.js

---
+++ controllers/controller.js
@@ -0,0 +1,64 @@
+//#region 
+// 電子支付跨機構共用平台
+const platform_get = (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'platform', 'index.html'));
+};
+
+const platform_code_get = (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'platform', 'code.html'));
+};
+
+//#endregion
+
+//#region 
+// Line Pay
+const line_pay_get = (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'line_pay', 'index.html'));
+};
+
+const line_pay_login_get = (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'line_pay', 'login.html'));
+};
+
+const line_pay_authentication_get = (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'line_pay', 'authentication.html'));
+};
+
+const line_pay_transfer_get = (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'line_pay', 'transfer.html'));
+};
+
+//#endregion
+
+//#region 
+// JKO Pay
+const jko_pay_get = (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'jko_pay', 'index.html'));
+};
+
+const jko_pay_login_get = (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'jko_pay', 'login.html'));
+};
+
+const jko_pay_authentication_get = (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'jko_pay', 'authentication.html'));
+};
+
+const jko_pay_transfer_get = (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'jko_pay', 'transfer.html'));
+};
+
+//#endregion
+
+module.exports = {
+  platform_get,
+  platform_code_get,
+  line_pay_get,
+  line_pay_login_get,
+  line_pay_authentication_get,
+  line_pay_transfer_get,
+  jko_pay_get,
+  jko_pay_login_get,
+  jko_pay_authentication_get,
+  jko_pay_transfer_get
+};
--- routes/router.js
+++ routes/router.js
@@ -1,48 +1,21 @@
 const express = require('express');
+const controller = require('../controllers/controller');
 const router = express.Router();
 
-
 // 電子支付跨機構共用平台
-router.get('/platform', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages', 'platform', 'index.html'));
-});
-
-router.get('/platform/code', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages', 'platform', 'code.html'));
-});
-
-// JKO Pay
-router.get('/jko_pay', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages', 'jko_pay', 'index.html'));
-});
-
-router.get('/jko_pay/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages', 'jko_pay', 'login.html'));
-});
-
-router.get('/jko_pay/authentication', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages', 'jko_pay', 'authentication.html'));
-});
-
-router.get('/jko_pay/transfer', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages', 'jko_pay', 'transfer.html'));
-});
+router.get('/platform', controller.platform_get);
+router.get('/platform/code', controller.platform_code_get);
 
 // Line Pay
-router.get('/line_pay', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages', 'line_pay', 'index.html'));
-});
+router.get('/line_pay', controller.line_pay_get);
+router.get('/line_pay/login', controller.line_pay_login_get);
+router.get('/line_pay/authentication', controller.line_pay_authentication_get);
+router.get('/line_pay/transfer', controller.line_pay_transfer_get);
 
-router.get('/line_pay/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages', 'line_pay', 'login.html'));
-});
-
-router.get('/line_pay/authentication', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages', 'line_pay', 'authentication.html'));
-});
-
-router.get('/line_pay/transfer', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages', 'line_pay', 'transfer.html'));
-});
+// JKO Pay
+router.get('/jko_pay', controller.jko_pay_get);
+router.get('/jko_pay/login', controller.jko_pay_login_get);
+router.get('/jko_pay/authentication', controller.jko_pay_authentication_get);
+router.get('/jko_pay/transfer', controller.jko_pay_transfer_get);
 
 module.exports = router;
